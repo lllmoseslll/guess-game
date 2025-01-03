0559de5d7181c1345e29fc0d3998b54ee3d8e361
lllmoseslll <mosespeter620@gmail.com>
2025-01-04 01:11:59 +0300

    styling button

--- App.js
+++ App.js
@@ -1,9 +1,22 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import StartGameScreen from "./screens/StartGameScreen";
+import { LinearGradient } from "expo-linear-gradient";
 
 export default function App() {
-  return <StartGameScreen />;
+  return (
+    <>
+      <StatusBar style="light" />
+      <LinearGradient colors={["#33021b", "#ff5602"]} style={styles.container}>
+        <StartGameScreen />;
+      </LinearGradient>
+    </>
+  );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#51142e",
+  },
+});
